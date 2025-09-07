f0798e756fbc78f9fdf384f6e070b6f7aa96a7e8
BeyondTheThreshold <rlgns4786@naver.com>
2025-09-07 18:12:47 +0900

    Implementing ladder animation and preventing double jump
    Create a command that's possible in a particular layer to prevent double jumping
    Invoke animators to synchronize the character's animations in certain situations
    And it modifies gravity so that it doesn't come down when you're on the ladder
    And if the speed is not input when riding the ladder, the animation stops within the ladder

--- Assets/Scripts/PlayMovement.cs
+++ Assets/Scripts/PlayMovement.cs
@@ -7,16 +7,26 @@ public class PlayMovement : MonoBehaviour
 {
     Vector2 moveInput;
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    Animator myAnimator;
     Rigidbody2D myRigidbody;
+    CapsuleCollider2D myCapsuleCollider;
+    float gravityScaleAtStart;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
+
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -25,10 +35,39 @@ public class PlayMovement : MonoBehaviour
         Debug.Log(moveInput);
     }
 
+    void OnJump(InputValue value)
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){return;}
+        if (value.isPressed)
+            {
+                myRigidbody.linearVelocity += new Vector2(0f, jumpSpeed);
+            }
+    }
+
+    void ClimbLadder()
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climb")))
+        //사다리 tilemap을 "climb"라는 layer로 조정하고 trigger로 해두면 된다. 
+        {
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            //사다리를 타고있을 때 흘러내려오는 걸 방지하기 위해 만든코드임
+            myAnimator.SetBool("isLadder", false);
+            return;
+        }
+        Vector2 climbVelocity = new Vector2(myRigidbody.linearVelocity.x, moveInput.y * climbSpeed);
+        myRigidbody.linearVelocity = climbVelocity;
+        myRigidbody.gravityScale = 0f;
+        //사다리를 타고있을 때로 인식되면 중력을 0으로해서 흘러내리지 않도록 방지 위의 코드는 원래대로 하는 코드임
+         bool playerHasVertical= Mathf.Abs(myRigidbody.linearVelocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isLadder", playerHasVertical);
+    }
+
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocity.y);
         myRigidbody.linearVelocity = playerVelocity;
+        bool playerHasHorizontal = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontal);
     }
 
     void FlipSprite()
